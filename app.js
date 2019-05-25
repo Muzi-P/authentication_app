d5bde03cb14db9bd09ccd7357a08f048c66e31dd
Muzi-P <phiwokuhlegondwe@gmail.com>
2019-05-26 01:44:21 +0800

    Users Model

--- app.js
+++ app.js
@@ -12,16 +12,17 @@ mongoose.connect(db, {useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected....'))
     .catch(err => console.log(err));
 
-
-const PORT = process.env.PORT || 5000;
-
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs')
+
+//Bodyparser
+app.use(express.urlencoded({ extended : false}));
+
 //Routes
 //Login Page
 app.use('/', require('./routes/index.js'))
 app.use('/users', require('./routes/users.js'))
 
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
